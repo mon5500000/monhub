5c417b8217742e815e40a2fc0ace4669a3c31423
mon5500000 <143809353+mon5500000@users.noreply.github.com>
2023-12-12 18:10:09 +0700

    Update tp.lua

--- tp.lua
+++ tp.lua
@@ -1,25 +1,27 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("MON HUB(ภาษาไทยเร็วๆนี้)", "DarkTheme")
 local Tab = Window:NewTab("tp")
-local Section = Tab:NewSection("Player")
+local Section = Tab:NewSection("ผู้เล่น")
 Plr = {}
 for i,v in pairs(game:GetService("Players"):GetChildren()) do
     table.insert(Plr,v.Name) 
 end
-local drop = Section:NewDropdown("Player", "RUOK no1", Plr, function(t)
+local drop = Section:NewDropdown("ผู้เล่น", "RUOK no1", Plr, function(t)
    PlayerTP = t
 end)
-Section:NewButton("Click To TP", "", function()
+Section:NewButton("คลิกเพื่อไปยัง TP", "", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
 end)
-Section:NewToggle("Auto Tp", "", function(t)
+Section:NewToggle("ออโต้ TP", "", function(t)
 _G.TPPlayer = t
 while _G.TPPlayer do wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[PlayerTP].Character.HumanoidRootPart.CFrame
 end
 end)
 
-Section:NewButton("Refresh","Refresh Dropdown", function()
+Section:NewButton("รีเฟรช","Refresh Dropdown", function()
   drop:Refresh(Plr)
 end)
 local Tab = Window:NewTab("BYsalmon.hub")
+local Tab = Window:NewTab("ภาษาไทยทํายาก")
+local Tab = Window:NewTab("ขี้เกียดทําละ")
