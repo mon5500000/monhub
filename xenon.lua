120eeb4f053e2a139ce0a738a7db99502a2e3e23
mon5500000 <143809353+mon5500000@users.noreply.github.com>
2024-02-24 09:13:36 +0700

    Add files via upload

---
+++ xenon.lua
@@ -0,0 +1,104 @@
+_G.Nexus = {
+    Main = {
+        Enabled = true,
+        Quests = {
+            ['Pull Lever'] = true,
+            ['Rainbow Haki'] = true,
+            ['Musketeer Hat'] = true,
+            ['Mirror Fractal'] = true
+        }
+    },
+    Sockets = {
+        Enabled = true,
+        MachineName = 'PC 1',
+        Sword = {
+            'Cursed Dual Katana',
+            'Dark Dagger',
+            'True Triple Katana',
+            'Tushita',
+            'Yama',
+            'Hallow Scythe'
+        },
+        Fruit = {
+            'Kitsune',
+            'Leopard',
+            'Dragon',
+            'Spirit',
+            'Control',
+            'Venom',
+            'Shadow',
+            'Dough',
+            'Mammoth',
+            'T-Rex'
+        }
+    },
+    Efficiency = {
+        ['FPS Booster'] = true,
+        ['Black Screen'] = false,
+    },
+    Fruits = {
+        ['Enabled'] = true,
+        ['Awakening Fruit'] = true,
+        ['List'] = {
+            'Quake-Quake',
+            'Dark-Dark',
+            'Spider-Spider',
+            'Rumble-Rumble',
+            'Magma-Magma',
+            'Buddha-Buddha',
+            'Dough-Dough',
+        },
+    },
+    Melee = {
+        ['6 Melee'] = true,
+    },
+    Sword = {
+        ['Pole'] = true,
+        ['Saber'] = true,
+        ['Midnight Blade'] = true,
+        ['Shisui'] = true,
+        ['Saddi'] = true,
+        ['Wando'] = true,
+        ['Yama'] = true,
+        ['Tushita'] = true,
+        ['Rengoku'] = true,
+        ['Canvander'] = true,
+        ['Buddy Sword'] = true,
+        ['Spikey Trident'] = true,
+        ['Twin Hooks'] = true,
+        ['Hallow Scryte'] = true,
+        ['True Triple Katana'] = true,
+        ['Cursed Dual Katana'] = true,
+    },
+    Gun = {
+        ['Kabucha'] = true,
+        ['Serpent Bow'] = true,
+        ['Soul Guitar'] = true
+    },
+    Race = {
+        Enabled = true,
+        Races = {
+            ['Mink'] = true,
+            ['Human'] = false,
+            ['Fishman'] = true,
+            ['Skypiea'] = true,
+        },
+        Specials = {
+            ['Ghoul'] = true,
+            ['Cyborg'] = true,
+        }
+    },
+    Mastery = {
+        Sword = {
+            ['Enabled'] = true,
+            ['Manual'] = true,
+            ['Sword Name'] = {
+                'True Triple Katana',
+                'Cursed Dual Katana',
+            }
+        },
+        DevilFruits = true
+    }
+}
+script_key="PlhVXEZsydamuOFgmFbODbcpzSvAmyAs";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenon-Trash/Nexus/main/Nexus.lua"))()
